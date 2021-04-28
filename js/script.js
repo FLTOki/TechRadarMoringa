85377f33cb76abc43cb616eeeae6a526de942217
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-28 10:23:43 +0300

    Completing functionality

--- js/script.js
+++ js/script.js
@@ -14,3 +14,64 @@ $(document).ready(function() {
       {
         $(".clickable_2").fadeIn("slow");
       });
+      //APPLE REVIEW SECTION END
+
+    //SAMSUNG REVIEW SECTION START
+    $(".first").mouseover(function()
+    {
+      $("#hide_first").fadeIn("slow");
+    });
+    $(".first").mouseleave(function()
+    {
+      $("#hide_first").fadeOut("slow");
+    });
+
+    $(".second").mouseover(function()
+    {
+      $("#hide_second").fadeIn("slow");
+    });
+    $(".second").mouseleave(function()
+    {
+      $("#hide_second").fadeOut("slow");
+    });
+
+    $(".third").mouseover(function()
+    {
+      $("#hide_third").fadeIn("slow");
+    });
+    $(".third").mouseleave(function()
+    {
+      $("#hide_third").fadeOut("slow");
+    });
+
+    $(".fourth").mouseover(function()
+    {
+      $("#hide_fourth").fadeIn("slow");
+    });
+    $(".fourth").mouseleave(function()
+    {
+      $("#hide_fourth").fadeOut("slow");
+    });
+
+    $(".fifth").mouseover(function()
+    {
+      $("#hide_fifth").fadeIn("slow");
+    });
+    $(".fifth").mouseleave(function()
+    {
+      $("#hide_fifth").fadeOut("slow");
+    });
+
+    $(".sixth").mouseover(function()
+    {
+      $("#hide_sixth").fadeIn("slow");
+    });
+    $(".sixth").mouseleave(function()
+    {
+      $("#hide_sixth").fadeOut("slow");
+    });
+
+    $('[data-toggle="popover"]').popover();
+
+    //SAMSUNG REVIEW SECTION END
+});
