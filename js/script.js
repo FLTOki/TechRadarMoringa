53086ac26a15c2174c7defcf4773c37f5e3c1a36
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-04-28 14:34:32 +0300

    reconfiguration

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-    //APPLE REVIEW SECTION START
-      $('#show_items').mouseover(function()
-      {
-        $(".clickable").fadeIn("slow");
-      });
-  
-      $("#show_mac").mouseover(function()
-      {
-        $("#hide_mac").fadeIn("slow");
-      });
-  
-      $("#hide_mac").mouseover(function()
-      {
-        $(".clickable_2").fadeIn("slow");
-      });
-      //APPLE REVIEW SECTION END
+  //APPLE REVIEW SECTION START
+    $('#show_items').mouseover(function()
+    {
+      $(".clickable").fadeIn("slow");
+    });
+
+    $("#show_mac").mouseover(function()
+    {
+      $("#hide_mac").fadeIn("slow");
+    });
+
+    $("#hide_mac").mouseover(function()
+    {
+      $(".clickable_2").fadeIn("slow");
+    });
+    //APPLE REVIEW SECTION END
 
     //SAMSUNG REVIEW SECTION START
     $(".first").mouseover(function()
